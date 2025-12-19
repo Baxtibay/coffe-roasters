72aa17ddf2599e7d3370a40c77650f92b4cc4bfb
Baxtibay <baxtibaybekniyazov@gmail.com>
2025-12-19 23:02:55 +0500

    Add site-header

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const elNavToggleBtn = document.querySelector('.site-header__toggle-btn');
+const elSiteHeader = document.querySelector('.site-header');
+
+
+if(elNavToggleBtn) { elNavToggleBtn.addEventListener('click', () => elSiteHeader.classList.toggle('site-header--open'))}
