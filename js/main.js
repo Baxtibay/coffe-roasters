53bbe0589eb405aa090d3e2146fce53654007acc
Baxtibay <baxtibaybekniyazov@gmail.com>
2026-01-01 14:34:47 +0500

    Add site-footer and relocation navbar to components folder

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ const elNavToggleBtn = document.querySelector('.site-header__toggle-btn');
 const elSiteHeader = document.querySelector('.site-header');
 
 
-if(elNavToggleBtn) { elNavToggleBtn.addEventListener('click', () => elSiteHeader.classList.toggle('site-header--open'))}
+if(elNavToggleBtn) { elNavToggleBtn.addEventListener('click', () => {
+  elSiteHeader.classList.toggle('site-header--open')
+  document.body.classList.toggle('menu-open');
+})}
